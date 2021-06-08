0983a844e1d85e46abe982b7516a57c35096eab4
LaizFarias <81171500+LaizFarias@users.noreply.github.com>
2021-06-08 17:53:51 -0300

    muda_jogador

--- muda_jogador.py
+++ muda_jogador.py
@@ -4,7 +4,29 @@ window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
 pygame.display.set_caption('Hunters')
 
-IMG1 = pygame.transform.scale(pygame.image.load("Graficos/fig1.jpg"), (pygame.display.get_surface().get_size()[0], pygame.display.get_surface().get_size()[1]))
-IMG2 = pygame.transform.scale(pygame.image.load("Graficos/fig2.jpg"), (pygame.display.get_surface().get_size()[0], pygame.display.get_surface().get_size()[1]))
-IMG3 = pygame.transform.scale(pygame.image.load("Graficos/menu.png"), (pygame.display.get_surface().get_size()[0], pygame.display.get_surface().get_size()[1]))
+
+
+IMG8 = pygame.image.load("Graficos/muda_jogador.jpeg")
+render_img8 = True
+state = "INIT"
+i = 0
+while state != "QUIT":
+    if i == 1: 
+        window.blit(IMG8, (0,0))
+    if i >= 2:
+        state = "GAME"
+        import Jogo
+        state = "QUIT"
+        break
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            state = "INFORMATIVO"
+            i+=1
+        if event.type == pygame.QUIT:
+            state = "QUIT"
+            break
+    pygame.display.update()
+pygame.quit() 
+    
+
 
